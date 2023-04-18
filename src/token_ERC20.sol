66166090aa39ff47af762eecc11a6614e08fb56f
MARCOS ALONSO CAMPILLO <marcal16@ucm.es>
2023-04-18 18:39:13 +0200

    imports

--- src/token_ERC20.sol
+++ src/token_ERC20.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity > 0.8.0;
 //Implementacion of the ERC20 Implementation
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contracts/token/ERC20/ERC20.sol";
-import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Stoken is ERC20, Ownable {
     
-    constructor(uint256 tokenAmount) ERC20("SmartTokenX", "STX") owner(msg.sender()) {
+    constructor(uint256 tokenAmount) ERC20("SmartTokenX", "STX") Ownable() {
         _mint(msg.sender, tokenAmount * (10 ** uint256(decimals())));
     }
 
