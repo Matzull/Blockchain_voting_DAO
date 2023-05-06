c0b66c1d53ffa454144f6cb03e5e21584b908927
leonnovacki <leon.novacki@gmail.com>
2023-05-06 02:28:07 +0200

    Adding a lot of proposal tests

--- test/voting_tests.js
+++ test/voting_tests.js
@@ -1,3 +1,5 @@
+// npx hardhat test test/voting_tests.js
+
 const {
   time,
   loadFixture,
@@ -18,7 +20,24 @@ describe("Voting", function () {
     const quadraticVoting_contract = await hre.ethers.getContractFactory("quadraticVoting");
     const quadraticVoting = await quadraticVoting_contract.deploy();
 
-    return { quadraticVoting, owner, voter, voter2 };
+    const proposal_contract = await hre.ethers.getContractFactory("Proposal");
+    const proposal = await proposal_contract.deploy();
+
+    return { quadraticVoting, owner, voter, voter2, proposal };
+  }
+
+  // Helper function, opens the voting in the quadraticVoting contract
+  async function ownerOpensVoting(quadraticVoting, owner) {
+    // test
+    const quadraticVoting_from_owner = await quadraticVoting.connect(owner);
+    quadraticVoting_from_owner.openVoting();
+  }
+
+  // Helper function, makes the voter become a participant, takes argument for which voter
+  async function voterBecomesParticipant(quadraticVoting, voter) {
+    const quadraticVoting_from_voter = await quadraticVoting.connect(voter);
+    let paid_for_token = ethers.utils.parseEther("0.0000000000003");
+    quadraticVoting_from_voter.addParticipant({value: paid_for_token});
   }
 
   describe("Deployment", function () {
@@ -59,7 +78,17 @@ describe("Voting", function () {
       await expect(quadraticVoting_from_voter2.addParticipant({value: not_enough_for_token})).to.be.reverted;
     })
 
+    // WORK IN PROGRESS
+    // it("removeParticipant(): Participant removes itself from the system, cannot function in the system anymore", async function() {
+    //   const { quadraticVoting, voter, voter2} = await loadFixture(deployVotingContract);
+    //   const quadraticVoting_from_voter = await quadraticVoting.connect(voter);
+    //   let paid_for_token = ethers.utils.parseEther("0.0000000000003");
+    //   // quadraticVoting_from_voter.addParticipant({value: paid_for_token});
+    //   await expect(quadraticVoting_from_voter.addParticipant({value: paid_for_token})).to.not.be.reverted;
+    //   // voted added himself successfully first
 
+    // })
+  
 
     //   it("Should receive and store the funds to lock", async function () {
     //     const { lock, lockedAmount } = await loadFixture(
@@ -145,5 +174,136 @@ describe("Voting", function () {
     //       );
     //     });
     //   });
+  });
+  describe("Proposals", function() {
+
+    /*
+    it("AddProposal(): Can only add a proposal if voting is open", async function() {
+      const {quadraticVoting, owner, voter, voter2, proposal } = await loadFixture(deployVotingContract);
+      voterBecomesParticipant(quadraticVoting, voter);
+      const quadraticVoting_from_voter = await quadraticVoting.connect(voter);
+
+      const proposal_from_voter = await proposal.connect(voter);
+      ownerOpensVoting(quadraticVoting, owner);
+
+      // OWNER OPENED VOTING, SHOULDN'T BE REVERTED, BUT IT DOES GET REVERTED? This is why I separated the test into 2 functions, it
+      // behaves weirdly sometimes, so it's best to test things separately.
+      await expect(quadraticVoting_from_voter.addProposal("title", "description", 0, proposal_from_voter.address)).to.be.reverted;
+      // owner opens voting
+      ownerOpensVoting(quadraticVoting, owner);
+      quadraticVoting_from_voter2 = await quadraticVoting.connect(voter);
+      await expect(quadraticVoting_from_voter2.addProposal("title", "description", 0, proposal_from_voter.address)).to.not.be.reverted;
+    })
+    */
+
+    it("AddProposal(): Cannot add a proposal if voting isnt open", async function() {
+      const {quadraticVoting, owner, voter, voter2, proposal } = await loadFixture(deployVotingContract);
+      voterBecomesParticipant(quadraticVoting, voter);
+      const quadraticVoting_from_voter = await quadraticVoting.connect(voter);
+      const proposal_from_voter = await proposal.connect(voter);
+      await expect(quadraticVoting_from_voter.addProposal("title", "description", 0, proposal_from_voter.address)).to.be.reverted;
+      quadraticVoting_from_voter2 = await quadraticVoting.connect(voter);
+      // await expect(quadraticVoting_from_voter2.addProposal("title", "description", 0, proposal_from_voter.address)).to.not.be.reverted;
+    })
+
+    it("AddProposal(): Can add a proposal if voting is open", async function() {
+      const {quadraticVoting, owner, voter, voter2, proposal } = await loadFixture(deployVotingContract);
+      ownerOpensVoting(quadraticVoting, owner);
+      voterBecomesParticipant(quadraticVoting, voter);
+      const quadraticVoting_from_voter = await quadraticVoting.connect(voter);
+      const proposal_from_voter = await proposal.connect(voter);
+      // await expect(quadraticVoting_from_voter.addProposal("title", "description", 0, proposal_from_voter.address)).to.be.reverted;
+      await expect(quadraticVoting_from_voter.addProposal("title", "description", 0, proposal_from_voter.address)).to.not.be.reverted;
+    })
+
+    it("AddProposal(): Successfully adds a signaling proposal", async function() {
+      // setup
+      const {quadraticVoting, owner, voter, voter2, proposal } = await loadFixture(deployVotingContract);
+      ownerOpensVoting(quadraticVoting, owner);
+      voterBecomesParticipant(quadraticVoting, voter);
+      // from voter
+      const quadraticVoting_from_voter = await quadraticVoting.connect(voter);
+      const proposal_from_voter = await proposal.connect(voter);
+      // adds a signaling proposal, 3rd argument is 0
+      await quadraticVoting_from_voter.addProposal("title", "description", 0, proposal_from_voter.address);
+      // first added proposal has an index of 1
+      let bigNumber1 = ethers.BigNumber.from(1);
+      expect((await quadraticVoting_from_voter.getSignalingProposals()).length).to.equal(bigNumber1);
+    })
+
+    it("AddProposal(): Successfully adds a funding proposal", async function() {
+      // setup
+      const {quadraticVoting, owner, voter, voter2, proposal } = await loadFixture(deployVotingContract);
+      ownerOpensVoting(quadraticVoting, owner);
+      voterBecomesParticipant(quadraticVoting, voter);
+      // from voter
+      const quadraticVoting_from_voter = await quadraticVoting.connect(voter);
+      const proposal_from_voter = await proposal.connect(voter);
+      // adds a signaling proposal, 3rd argument is 0
+      await quadraticVoting_from_voter.addProposal("title", "description", 20, proposal_from_voter.address);
+      // first added proposal has an index of 1
+      let bigNumber1 = ethers.BigNumber.from(1);
+      expect((await quadraticVoting_from_voter.getPendingProposals()).length).to.equal(bigNumber1);
+    })
+
+    it("getSignalingProposals(): Returns empty array if no proposals", async function() {
+      // setup
+      const {quadraticVoting, owner, voter, voter2, proposal } = await loadFixture(deployVotingContract);
+      ownerOpensVoting(quadraticVoting, owner);
+      voterBecomesParticipant(quadraticVoting, voter);
+      // from voter
+      const quadraticVoting_from_voter = await quadraticVoting.connect(voter);
+      expect((await quadraticVoting_from_voter.getSignalingProposals()).length).to.equal(0);
+    })
+
+    it("getPendingProposals(): Returns empty array if no proposals", async function() {
+      // setup
+      const {quadraticVoting, owner, voter, voter2, proposal } = await loadFixture(deployVotingContract);
+      ownerOpensVoting(quadraticVoting, owner);
+      voterBecomesParticipant(quadraticVoting, voter);
+      // from voter
+      const quadraticVoting_from_voter = await quadraticVoting.connect(voter);
+      expect((await quadraticVoting_from_voter.getPendingProposals()).length).to.equal(0);
+    })
+
+    // Needs to change if we change getProposalInfo()
+    it("GetProposalInfo(): Returns proposal info", async function() {
+      // setup
+      const {quadraticVoting, owner, voter, voter2, proposal } = await loadFixture(deployVotingContract);
+      ownerOpensVoting(quadraticVoting, owner);
+      voterBecomesParticipant(quadraticVoting, voter);
+      // from voter
+      const quadraticVoting_from_voter = await quadraticVoting.connect(voter);
+      const proposal_from_voter = await proposal.connect(voter);
+      // adds a funding proposal of budget 20
+      budget = 20;
+      BN_budget = ethers.BigNumber.from(budget);
+      await quadraticVoting_from_voter.addProposal("title", "description", budget, proposal_from_voter.address);
+      // first added proposal has an index of 1
+      expect((await quadraticVoting_from_voter.getProposalInfo(1))).to.equal(BN_budget);
+    })
+
+    it("CancelProposal(): Only the creator of a proposal can cancel it", async function() {
+      // setup
+      const {quadraticVoting, owner, voter, voter2, proposal } = await loadFixture(deployVotingContract);
+      ownerOpensVoting(quadraticVoting, owner);
+      voterBecomesParticipant(quadraticVoting, voter);
+      // from voter
+      const quadraticVoting_from_voter = await quadraticVoting.connect(voter);
+      const proposal_from_voter = await proposal.connect(voter);
+      // voter adds a proposals
+      await quadraticVoting_from_voter.addProposal("title", "description", 0, proposal_from_voter.address);
+
+      // voter2 tries to cancel it, he cannot
+      voterBecomesParticipant(quadraticVoting, voter2);
+      const quadraticVoting_from_voter2 = await quadraticVoting.connect(voter2);
+      expect(await quadraticVoting_from_voter.cancelProposal(1)).to.be.reverted;
+
+      // voter cancels it
+      expect(await quadraticVoting_from_voter.cancelProposal(1)).to.not.be.reverted;
+    })
+
+
+
   });
 });
